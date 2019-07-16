af10741491ea19c028ba52161b65ae00c53e92e7
LiuJiJiang <1357885013@qq.com>
2019-07-16 20:38:54 +0800

    Initial commit

---
+++ classTest/Man.java
@@ -0,0 +1,29 @@
+package classTest;
+public class Man{
+	public String name;
+	public String sex;
+	public static int count;
+	public double tall;
+	public Man(String name,String sex,double d) {
+		this.name=name;
+		this.sex=sex;
+		this.tall=d;
+		this.count++;
+	}
+	public Man(String name,String sex) {
+		this.name=name;
+		this.sex=sex;
+		this.count++;
+
+	}
+	public Man(String name) {
+		this.name=name;
+		this.count++;
+	}
+	public void eat(){
+		System.out.println("eat");
+	}
+	public void sleep(){
+		System.out.println("sleep");
+	}
+}
