2dbc089f96b5024f9febe92fd7f0d0b06bdd752b
LiuJiJiang <1357885013@qq.com>
2019-07-18 17:13:15 +0800

    del useless files

--- out/production/myCode/WordExtra/test.js
+++
@@ -1,6 +0,0 @@
-this
- is 
-a 
-javaScript object
-共扫描单词:5个[a, is, java, object, script] 5共扫描单词:10个[a, is, java, object, script] 5共扫描单词:15个[a, is, java, object, script] 5
-共扫描单词:20个[a, is, java, object, script] 5
