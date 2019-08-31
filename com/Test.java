65d182fe9e316f42c095c22cd9674f59ed81bf03
LiuJiJiang <1357885013@qq.com>
2019-08-31 17:26:14 +0800

    day day up

---
+++ com/Test.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Test {
+    @org.junit.Test
+    public  void test(){
+        System.out.println( 11 - 1 >> 1);
+        System.out.println( 10 - 1 >> 1);
+        System.out.println( 1024 >> 1);
+        System.out.println( 1024 >> 2);
+        System.out.println( 1024 << 1);
+        System.out.println( 1024 << 2);
+    }
+}
