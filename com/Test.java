688fbe83b7d3ccbcac90717936d89459e778b9bb
LiuJiJiang <1357885013@qq.com>
2019-09-12 16:23:42 +0800

    Update Test.java

--- com/Test.java
+++ com/Test.java
@@ -2,18 +2,18 @@ package com;
 
 public class Test {
     @org.junit.Test
-    public  void test(){
-        System.out.println( 11 - 1 >> 1);
-        System.out.println( 10 - 1 >> 1);
-        System.out.println( 1024 >> 1);
-        System.out.println( 1024 >> 2);
-        System.out.println( 1024 << 1);
-        System.out.println( 1024 << 2);
+    public void test() {
+        System.out.println(11 - 1 >> 1);
+        System.out.println(10 - 1 >> 1);
+        System.out.println(1024 >> 1);
+        System.out.println(1024 >> 2);
+        System.out.println(1024 << 1);
+        System.out.println(1024 << 2);
     }
 
     @org.junit.Test
-    public void test2(){
-        int[] a = new int[]{1,2,3,4};
+    public void test2() {
+        int[] a = new int[]{1, 2, 3, 4};
         System.out.println("a.getClass().getTypeName() = " + a.getClass().getTypeName());
     }
 }
